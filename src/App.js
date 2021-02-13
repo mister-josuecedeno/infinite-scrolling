5068bc7aee534be607c829ae9a64a77d5dc1045d
josuecedeno <josuecedeno@yahoo.com>
2021-02-12 21:31:03 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useRef, useCallback } from 'react';
 import './App.css';
+import useBookSearch from './hooks/useBookSearch';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          // console.log('visible');
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+      console.log(node);
+    },
+    [loading, hasMore]
+  );
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className='container'>
+        <h1 className='title'>Book Search</h1>
+        <input
+          className='search'
+          type='text'
+          value={query}
+          placeholder='Add your search term here'
+          onChange={handleSearch}
+        ></input>
+        {books.map((book, index) => {
+          if (books.length === index + 1) {
+            return (
+              <div className='result' ref={lastBookElementRef} key={book}>
+                {book}
+              </div>
+            );
+          } else {
+            return (
+              <div className='result' key={book}>
+                {book}
+              </div>
+            );
+          }
+        })}
+        <div className='loading'>{loading && 'Loading...'}</div>
+        <div className='error'>{error && 'Error'}</div>
+      </div>
+    </Fragment>
   );
 }
 
